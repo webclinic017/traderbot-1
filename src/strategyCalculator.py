9f3792137b3a1306c3b737dd7a72a10b05cf3848
marcus418 <56596363+marcus418@users.noreply.github.com>
2020-09-06 17:10:49 +0800

    made alot of amendments

---
+++ src/strategyCalculator.py
@@ -0,0 +1,18 @@
+import Pyro4
+import time
+@Pyro4.expose
+class StrategyCalculator(object):
+
+    @Pyro4.oneway
+    def calculateStrategy(self,name,date):
+        print("Calculating Strategy for " + name + "...")
+        time.sleep(10)
+        print("Calculated Strategy for " + name + "...")
+
+daemon = Pyro4.Daemon()                # make a Pyro daemon
+ns = Pyro4.locateNS()                  # find the name server
+uri = daemon.register(StrategyCalculator)   # register the greeting maker as a Pyro object
+ns.register("strategy.calculator", uri)   # register the object with a name in the name server
+
+print("Ready.")
+daemon.requestLoop()                   # start the event loop of the server to wait for calls
