f14eddb81f4321d24f22ea391773f5ffda3d89fe
marcus418 <56596363+marcus418@users.noreply.github.com>
2020-09-24 12:53:45 +0800

    added indicators, added minimum profit to account for spread fees

--- src/indicators/indicators.py
+++ src/indicators/indicators.py
@@ -1,5 +1,5 @@
 
-from talib import MACDFIX, RSI, EMA, SAR, SMA, TRIX
+from talib import MACDFIX, RSI, EMA, SAR, SMA, TRIX, BBANDS, CDLIDENTICAL3CROWS, CDL3BLACKCROWS, CDL3WHITESOLDIERS, CDLMORNINGSTAR, CDLEVENINGSTAR, CDL3LINESTRIKE, CDLMORNINGDOJISTAR, CDLEVENINGDOJISTAR, CDL3OUTSIDE, CDLENGULFING, CDLBELTHOLD, CDLABANDONEDBABY, CDL3INSIDE, CDLPIERCING, CDLDARKCLOUDCOVER, CDLBREAKAWAY,CDLXSIDEGAP3METHODS, CDLHAMMER, CDLSHOOTINGSTAR
 import indicators.ATRcalc as atrcalc
 import os
 
@@ -9,7 +9,7 @@ class Indicator:
     # indicatorlist = ['ichimoku200', 'macdRSI', 'parabolic200', 'SMA200']    
     def beginCalc(self, df, tickerName):
         ## FILL THIS IN AS MORE INDICATORS ARE ADDED
-        indicatorlist = ['ichimoku200','macdRSI', 'macd200', 'parabolic200', 'SMA200', 'trix200', 'macdTRIX']
+        indicatorlist = ['ichimoku200','macdRSI', 'macd200', 'parabolic200', 'SMA200', 'trix200', 'macdTRIX', 'bbands200']
 
         # Step 1: Find out if analysis csv exists
         
@@ -198,21 +198,30 @@ class Indicator:
         elif marketCloud == -1 and marketEMA < 0 and AheadCloud <= 0 and crossover < 0:
             position = -1 ##short
         else: position = 0 ## no position
-
+        amount = 50
         if position == 1:
             closeKijunDistance = priceclose - CurrentKijun
             adjustedDistance = 1.05 * closeKijunDistance
             stoploss = priceclose - adjustedDistance
-            amount = priceclose / (priceclose-stoploss)
+            # amount = priceclose / (priceclose-stoploss)
             takeprofit = priceclose + 1.45*(priceclose - stoploss)
+            if (takeprofit - priceclose) * amount < 0.1:
+                amount = 0
+                position = 0
+                stoploss = 0
+                takeprofit = 0
 
         elif position == -1:
             closeKijunDistance = CurrentKijun - priceclose
             adjustedDistance = 1.05 * closeKijunDistance
             stoploss = priceclose + adjustedDistance
-            amount = priceclose / (stoploss - priceclose)
+            # amount = priceclose / (stoploss - priceclose)
             takeprofit = priceclose - 1.45*(stoploss - priceclose)
-
+            if(priceclose - takeprofit) * amount < 0.1:
+                amount = 0
+                position = 0
+                stoploss = 0
+                takeprofit = 0
         else: 
             amount = 0
             stoploss = 0
@@ -276,14 +285,26 @@ class Indicator:
         elif crossover < 0 and rsi[-1] >= 50 and macd[-1] > 0: position = -1
         else: position = 0
 
+        amount = 50
         if position == 1:
             stoploss = priceclose - 1.05 * atr
             takeprofit = priceclose + 1.45 * atr
-            amount = priceclose / (priceclose - stoploss)
+            # amount = priceclose / (priceclose - stoploss)
+            if (priceclose - stoploss) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
+
         elif position == -1:
             stoploss = priceclose + 1.05*atr
             takeprofit = priceclose - 1.45 * atr
-            amount = priceclose / (stoploss - priceclose)
+            # amount = priceclose / (stoploss - priceclose)
+            if(stoploss - priceclose) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
         else:
             stoploss = 0
             takeprofit = 0
@@ -351,14 +372,26 @@ class Indicator:
         elif marketEMA == -1 and change == -1: position = -1
         else: position = 0
 
+        amount = 50
+
         if position == 1:
             stoploss = sarCurrent[-1]
             takeprofit = priceclose + 1.45*(priceclose - sarCurrent[-1])
-            amount = priceclose / (priceclose - stoploss)
+            # amount = priceclose / (priceclose - stoploss)
+            if(priceclose - stoploss) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
         elif position == -1:
             stoploss = sarCurrent[-1]
             takeprofit = priceclose - 1.45 * (sarCurrent[-1] - priceclose)
-            amount = priceclose / (stoploss - priceclose)
+            # amount = priceclose / (stoploss - priceclose)
+            if(stoploss - priceclose) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
         else:
             stoploss = 0
             takeprofit = 0
@@ -423,15 +456,25 @@ class Indicator:
         else: position = 0
 
         atr = atrcalc.ATRcalc(df)
-
+        amount = 50
         if position == 1:
             stoploss = priceclose - 1.05 * atr
             takeprofit = priceclose + 1.45 * atr
-            amount = priceclose / (priceclose - stoploss)
+            # amount = priceclose / (priceclose - stoploss)
+            if(priceclose- stoploss) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
         elif position == -1:
             stoploss = priceclose + 1.05*atr
             takeprofit = priceclose - 1.45 * atr
-            amount = priceclose / (stoploss - priceclose)
+            # amount = priceclose / (stoploss - priceclose)
+            if(stoploss - priceclose) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
         else:
             stoploss = 0
             takeprofit = 0
@@ -502,15 +545,25 @@ class Indicator:
         if marketEMA == 1 and crossover > 0  and macd[-1] < 0: position = 1
         elif marketEMA == -1 and crossover < 0  and macd[-1] > 0: position = -1
         else: position = 0
-
+        amount = 50
         if position == 1:
             stoploss = priceclose - 1.05 * atr
             takeprofit = priceclose + 1.45 * atr
-            amount = priceclose / (priceclose - stoploss)
+            # amount = priceclose / (priceclose - stoploss)
+            if(priceclose - stoploss) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
         elif position == -1:
             stoploss = priceclose + 1.05*atr
             takeprofit = priceclose - 1.45 * atr
-            amount = priceclose / (stoploss - priceclose)
+            # amount = priceclose / (stoploss - priceclose)
+            if(stoploss - priceclose) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
         else:
             stoploss = 0
             takeprofit = 0
@@ -562,15 +615,25 @@ class Indicator:
         if marketEMA == 1 and crossover > 0: position = 1
         elif marketEMA == -1 and crossover < 0: position = -1
         else: position = 0
-
+        amount = 50
         if position == 1:
             stoploss = priceclose - 1.05 * atr
             takeprofit = priceclose + 1.45 * atr
-            amount = priceclose / (priceclose - stoploss)
+            # amount = priceclose / (priceclose - stoploss)
+            if(priceclose - stoploss) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
         elif position == -1:
             stoploss = priceclose + 1.05*atr
             takeprofit = priceclose - 1.45 * atr
-            amount = priceclose / (stoploss - priceclose)
+            # amount = priceclose / (stoploss - priceclose)
+            if(stoploss - priceclose) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
         else:
             stoploss = 0
             takeprofit = 0
@@ -615,15 +678,25 @@ class Indicator:
         if delayedmacd[-1] < delayedmacdsignal[-1] and macd[-1] > macdsignal[-1] and trixOutput[-1] < 0 : position = 1
         elif delayedmacd[-1] > delayedmacdsignal[-1] and macd[-1] < macdsignal[-1] and trixOutput[-1] > 0: position = -1
         else: position = 0
-
+        amount = 50
         if position == 1:
             stoploss = priceclose - 1.05 * atr
             takeprofit = priceclose + 1.45 * atr
-            amount = priceclose / (priceclose - stoploss)
+            # amount = priceclose / (priceclose - stoploss)
+            if(priceclose - stoploss) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
         elif position == -1:
             stoploss = priceclose + 1.05*atr
             takeprofit = priceclose - 1.45 * atr
-            amount = priceclose / (stoploss - priceclose)
+            # amount = priceclose / (stoploss - priceclose)
+            if(stoploss - priceclose) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
         else:
             stoploss = 0
             takeprofit = 0
@@ -633,3 +706,100 @@ class Indicator:
         # position = 1
         return [position, amount, priceclose, stoploss, takeprofit, 1]
 
+    def bbands200(self,df):
+        df = df.dropna()
+
+        #1. Calculate ATR for potential trade
+        atr = atrcalc.ATRcalc(df)
+
+        ## a. checkpatterns
+        cdlInput = df.head(20)
+        cdlInput = cdlInput.iloc[::-1]
+        aa = cdlInput['open'].values
+        ab = cdlInput['high'].values
+        ac = cdlInput['low'].values
+        ad = cdlInput['close'].values
+        outputI3C = CDLIDENTICAL3CROWS(aa,ab,ac,ad)
+        output3BC = CDL3BLACKCROWS(aa,ab,ac,ad)
+        output3WS = CDL3WHITESOLDIERS(aa,ab,ac,ad)
+        outputMS = CDLMORNINGSTAR(aa,ab,ac,ad)
+        outputES = CDLEVENINGSTAR(aa,ab,ac,ad)
+        output3LS = CDL3LINESTRIKE(aa,ab,ac,ad)
+        outputMDS = CDLMORNINGDOJISTAR(aa,ab,ac,ad)
+        outputEDS = CDLEVENINGDOJISTAR(aa,ab,ac,ad)
+        output3O = CDL3OUTSIDE(aa,ab,ac,ad)
+        outputE = CDLENGULFING(aa,ab,ac,ad)
+        outputBH = CDLBELTHOLD(aa,ab,ac,ad)
+        outputAB = CDLABANDONEDBABY(aa,ab,ac,ad)
+        output3I = CDL3INSIDE(aa,ab,ac,ad)
+        outputP = CDLPIERCING(aa,ab,ac,ad)
+        outputCDD = CDLDARKCLOUDCOVER(aa,ab,ac,ad)
+        outputB = CDLBREAKAWAY(aa,ab,ac,ad)
+        outputXSG3M = CDLXSIDEGAP3METHODS(aa,ab,ac,ad)
+        outputH = CDLHAMMER(aa,ab,ac,ad)
+        outputSS = CDLSHOOTINGSTAR(aa,ab,ac,ad)
+
+        if outputI3C[-1]>0 or output3BC[-1]>0 or output3WS[-1]>0 or outputMS[-1]>0 or outputES[-1]>0 or output3LS[-1]>0 or outputMDS[-1]>0 or outputEDS[-1]>0 or output3O[-1]>0 or outputE[-1]>0 or outputBH[-1]>0 or outputAB[-1]>0 or output3I[-1]>0 or outputP[-1]>0 or outputCDD[-1]>0 or outputB[-1]>0 or outputXSG3M[-1]>0 or outputH[-1]>0 or outputSS[-1]>0:
+            pattern = 1
+        elif  outputI3C[-1]<0 or output3BC[-1]<0 or output3WS[-1]<0 or outputMS[-1]<0 or outputES[-1]<0 or output3LS[-1]<0 or outputMDS[-1]<0 or outputEDS[-1]<0 or output3O[-1]<0 or outputE[-1]<0 or outputBH[-1]<0 or outputAB[-1]<0 or output3I[-1]<0 or outputP[-1]<0 or outputCDD[-1]<0 or outputB[-1]<0 or outputXSG3M[-1]<0 or outputH[-1]<0 or outputSS[-1]<0:
+            pattern = -1
+        else: pattern = 0
+
+        ##b. get BBands
+        bband = df.head(21)
+        bband = bband.iloc[1:]
+        bband = bband.iloc[::-1]
+        bbandInput = bband['close'].values
+        upperband, middleband, lowerband = BBANDS(bbandInput, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+
+        ##c. 200EMA
+        emaInput = df.head(200)
+        emaInput = emaInput.iloc[::-1]
+        EMAclose = emaInput['close'].values
+        ema = EMA(EMAclose, timeperiod=200)
+
+        ##d. current price action
+        priceaction = df.head(1)
+        pricehigh = priceaction['high'].values[0]
+        pricelow = priceaction['low'].values[0]
+        priceclose = priceaction['close'].values[0]
+
+        if pricehigh > upperband[-1]: breakBand = -1
+        elif pricelow < lowerband[-1]: breakBand = 1
+        else: breakBand = 0
+
+        if pricelow > ema[-1]: marketEMA = 1
+        elif pricehigh < ema[-1]: marketEMA = -1
+        else: marketEMA = 0
+
+        ##OUTPUT
+        if marketEMA == 1 and pattern == 1 and breakBand == 1: position = 1
+        elif marketEMA == -1 and pattern == -1 and breakBand == -1: position = -1
+        else: position = 0
+        amount = 50
+        if position == 1:
+            stoploss = priceclose - 1.05 * atr
+            takeprofit = priceclose + 1.45 * atr
+            # amount = priceclose / (priceclose - stoploss)
+            if(stoploss - priceclose) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
+                
+        elif position == -1:
+            stoploss = priceclose + 1.05*atr
+            takeprofit = priceclose - 1.45 * atr
+            # amount = priceclose / (stoploss - priceclose)
+            if(stoploss - priceclose) * amount < 0.1:
+                position = 0
+                amount = 0
+                stoploss = 0
+                takeprofit = 0
+
+        else:
+            stoploss = 0
+            takeprofit = 0
+            amount = 0
+
+        return [position, amount, priceclose, stoploss, takeprofit, 1]
